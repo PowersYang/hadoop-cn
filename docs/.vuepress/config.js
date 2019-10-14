abf4014f06bfa93280fdadac7d9f3f868c9efaf4
Y-sir <577790911@qq.com>
2019-10-14 14:23:32 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,14 +7,14 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },                      // 根路径
-      { text: '下载', link: 'https://hadoop.apache.org/releases.html' },
-      { text: '文档', link: 'https://google.com' }, // 外部链接
-      // 显示下拉列表
+      { text: '下载安装', link: 'https://hadoop.apache.org/releases.html' },
+      { text: '参考文档', link: 'https://google.com' }, // 外部链接
+      { text: '资源教程', link: '/docs/awesome/'},
       {
-        text: '语言',
+        text: '语言支持',
         items: [
-          { text: '中文', link: '/language/ch' },
-          { text: '英文', link: '/language/en' }
+          { text: '简体中文', link: '/language/ch' },
+          { text: 'English', link: '/language/en' }
         ]
       },
       {text: '关于', link: '/about/'}
