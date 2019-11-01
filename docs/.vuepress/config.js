ccd8cb803eb0e9d4355e8e739477d8fedd441126
杨旺强 <577790911@qq.com>
2019-11-01 17:21:59 +0800

    '添加英文md'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,9 +6,25 @@ module.exports = {
   ],  
   themeConfig: {
     nav: [
-      { text: '首页', link: '/' },                      // 根路径
+      { text: '首页', link: '/' },                      
       { text: '下载安装', link: 'https://hadoop.apache.org/releases.html' },
-      { text: '参考文档', link: 'https://google.com' }, // 外部链接
+      { text: '参考文档', 
+		items: [
+			{ text: '通用', link: ''},
+			{ text: '公共', link: ''},
+			{ text: 'HDFS', link: ''},
+			{ text: 'MapReduce', link: ''},
+			{ text: 'MapReduce REST APIs', link: ''},
+			{ text: 'YARN REST APIs', link: ''},
+			{ text: 'YARN Service', link: ''},
+			{ text: 'Submarine', link: ''},
+			{ text: 'Hadoop兼容文件系统', link: ''},
+			{ text: 'Auth', link: ''},
+			{ text: '工具', link: ''},
+			{ text: '参考', link: ''},
+			{ text: '配置', link: ''},
+		]
+	  },
       { text: '资源教程', link: '/docs/awesome/'},
       {
         text: '语言支持',
@@ -18,6 +34,7 @@ module.exports = {
         ]
       },
       {text: '关于', link: '/about/'}
-    ]
+    ],
+	sidebar: 'auto'
   }
 }
