f0b6f193ef2367d4537313bd394bc99e083a823a
Y-sir <577790911@qq.com>
2019-11-03 09:58:32 +0800

    添加回到顶部

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,63 +11,45 @@ module.exports = {
       description: 'Hadoop Chinese Website, Hadoop Chinese Community.'
     }
   },
-  
+
   themeConfig: {
     logo: '/hadoop.jpeg',
+    repo: 'Y-sir/hadoop-cn',
+    editLinks: true,
+    docsDir: 'docs',
+    smoothScroll: true,
     locales: {
       '/': {
         sidebar: require('./sidebar/zh')(),
-        repo: 'Y-sir/hadoop-cn',
-        editLinks: true,
-        docsDir: 'docs',
         lastUpdated: '最后更新',
-        smoothScroll: true,
         selectText: '选择语言',
         label: '简体中文',
-        ariaLabel: '选择语言',
         editLinkText: '在 GitHub 上编辑此页',
-        serviceWorker: {
-          updatePopup: {
-            message: "发现新内容可用.",
-            buttonText: "刷新"
-          }
-        },
         nav: [
           { text: '首页', link: '/' },
           { text: '下载安装', link: '/docs/releases/download' },
           { text: '参考文档', link: '/docs/' },
           { text: '资源教程', link: '/docs/awesome/' },
-          { text: '最新消息', link: '/docs/releases/latestnews'},
+          { text: '最新消息', link: '/docs/releases/latestnews' },
           { text: '关于', link: '/about/' }
         ],
       },
       '/en/': {
         sidebar: require('./sidebar/en')(),
-        repo: 'Y-sir/hadoop-cn',
-        editLinks: true,
-        docsDir: '/docs/en/',
         lastUpdated: 'Last Updated',
-        smoothScroll: true,
         selectText: 'Languages',
         label: 'English',
         editLinkText: 'Edit this page on GitHub',
-        serviceWorker: {
-          updatePopup: {
-            message: "New content is available.",
-            buttonText: "Refresh"
-          }
-        },
         nav: [
           { text: 'Home', link: '/' },
           { text: 'Download', link: '/en/releases/download' },
           { text: 'Documentation', link: '/en/docs/' },
           { text: 'Awesome', link: '/en/awesome/' },
-          { text: 'Latest News', link: '/en/docs/releases/latestnews'},
+          { text: 'Latest News', link: '/en/docs/releases/latestnews' },
           { text: 'About', link: '/en/about/' }
         ],
       }
     }
-
   },
 
   plugins: ['@vuepress/back-to-top']
---
+++ docs/docs/releases/latestnews.md
@@ -0,0 +1,43 @@
+---
+sidebar: false
+---
+
+::: tip Release 2.10.0 available <span style="float:right">2019 Oct 29</span>
+
+This is the first stable release of Apache Hadoop 2.10 line. It contains 362 bug fixes, improvements and enhancements since 2.9.0.
+
+Users are encouraged to read the overview of major changes since 2.9.0. For details of 362 bug fixes, improvements, and other enhancements since the previous 2.9.0 release, please check release notes and changelog detail the changes since 2.9.0.
+
+:::
+
+::: tip Release 3.1.3 available <span style="float:right">2019 Oct 21</span>
+
+This is the third stable release of Apache Hadoop 3.1 line. It contains 246 bug fixes, improvements and enhancements since 3.1.2.
+
+Users are encouraged to read the overview of major changes since 3.1.2. For details of the bug fixes, improvements, and other enhancements since the previous 3.1.2 release, please check release notes and changelog
+
+:::
+
+::: tip Ozone 0.4.1-alpha is released <span style="float:right">2019 Oct 13</span>
+
+Next version of Apache Hadoop Ozone is released with Native ACLs, K8s support and improved stability.
+
+For more information check the ozone site.
+
+:::
+
+::: tip Release 3.2.1 available <span style="float:right">2019 Sep 22</span>
+
+This is the second stable release of Apache Hadoop 3.2 line. It contains 493 bug fixes, improvements and enhancements since 3.2.0
+
+Users are encouraged to read the overview of major changes since 3.2.0 For details of 493 bug fixes, improvements, and other enhancements since the previous 3.2.0 release, please check release notes and changelog detail the changes since 3.2.0
+
+:::
+
+::: tip Ozone 0.4.0-alpha is released <span style="float:right">2019 May 7</span>
+
+Ozone 0.4.0 alpha version supports kerberos and transparent data encryption. This is first secure Ozone release. It is compatible with apache Spark, Hive and Yarn.
+
+For more information check the ozone site.
+
+:::
